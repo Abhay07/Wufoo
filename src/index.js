625f50f20eacb3da7c1790589357900fd7662bfa
abhay-srivastav <abhaysrivastav007@gmail.com>
2019-04-27 01:34:24 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -21,17 +21,18 @@ app.post('/upload',(req,res,next)=>{
 		'autopilotapikey':key,
 		'Content-Type':'application/json'
 	}
+	console.log(req.body);
 	const data={
 		"contact":{
-			"FirstName":req.body.Field1,
+			"Company":req.body.Field1,
 			"Lastname":req.body.Field2,
 			"Email":req.body.Field10,
+			"Company":req.body.
 			"_autopilot_list":"#contactlist_72E57C91-3889-46EA-A304-0703CD9C64EE"
 		}
 	}
 	axios.post('https://api2.autopilothq.com/v1/contact',data,{headers:headers})
 	.then((response)=>{
-		console.log(response.data);
 	})
 	.catch((error)=>{
 		console.log(error);
