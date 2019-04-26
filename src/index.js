953f28d692c5bff0988f1c321b631289ff29884d
abhay-srivastav <abhaysrivastav007@gmail.com>
2019-04-27 00:55:29 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios');
+const config = require('../config');
+const key = config.secret;
 
 
 const app = express()
@@ -14,8 +16,25 @@ app.use(function (err, req, res, next) {
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-
+const 
 app.post('/upload',(req,res,next)=>{
-	console.log(req.body);
+	const headers = {
+		'autopilotapikey':key
+		'Content-Type':'application/json'
+	}
+	const data={
+		"contact":{
+			"FirstName":req.body.Field1,
+			"Lastname":req.body.Field2,
+			"Email":req.body.Field10
+		}
+	}
+	axios.post('https://api2.autopilothq.com/v1/contact',data,{headers:headers})
+	.then((response)=>{
+		console.log(response.data);
+	})
+	.catch((error)=>{
+		console.log(error);
+	})
 	res.send('test');
 })
